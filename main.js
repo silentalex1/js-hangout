dcaf42bf52183a1fcb15ab3d68616ddeaacc54c3
Zal <101911964+silentalex1@users.noreply.github.com>
2025-10-30 19:07:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const usernameDisplay = document.getElementById('username-display');
+    const logoutBtn = document.getElementById('logout-btn');
 
+    const loggedInUser = sessionStorage.getItem('loggedInUser');
+
+    if (!loggedInUser) {
+        window.location.href = 'index.html';
+    } else {
+        usernameDisplay.textContent = loggedInUser;
+    }
+
+    logoutBtn.addEventListener('click', () => {
+        sessionStorage.removeItem('loggedInUser');
+        window.location.href = 'index.html';
+    });
+});
